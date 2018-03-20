f167751d683e9e51e7baddf742693b53d48467ea
Lena Scholz <lscholz@uos.de>
2018-03-20 11:31:59 +0100

    Expand DataPane to allow for different types and multiple values

--- src/main/java/uos/lscholz/gedenkstaettendb/view/DataPane.java
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/DataPane.java
@@ -1,40 +1,90 @@
 package uos.lscholz.gedenkstaettendb.view;
 
 import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * TODO: possibly turn this into separate classes in the future
+ *
+ * TODO: create separate classes for delete and add button
  */
 public class DataPane extends GridPane{
 
     private String type;
     private Label label;
-    private Node content;
+    private List<Node> contentNodes;
+    private Button addData;
+    private List<Button> dataDeletes;
+    private Separator separator;
 
 
-    public DataPane(String label, String type, String content){
+    public DataPane(String label, String type, String [] contents){
+        this.contentNodes = new LinkedList<Node>();
+        this.dataDeletes = new LinkedList<Button>();
         this.type = type;
         this.label = new Label(label);
+        this.addData = new Button("+");
+        this.addData.setId("addButton");
+        this.add(addData,1,1);
+        this.add(this.label,2,1);
+        handleContent(type,contents);
+        this.separator = new Separator();
+        this.add(separator,1,(3+this.contentNodes.size()),2,1);
     }
 
-    private void handleContent(String type, String content){
-        switch (type){
-            case "String":;break;
-            case "Integer":;break;
-            case "Boolean":;break;
-            default:throw new RuntimeException("Typ wird nicht unterstützt");
+    private void handleContent(String type, String [] contents){
 
+        for(int i = 0; i< contents.length; i++){
+            this.dataDeletes.add(new Button("X"));
+            this.dataDeletes.get(i).setId("deleteButton");
+            Node node;
+            switch (type){
+                case "TEXT": node = handleText(contents[i]);break;
+                case "DATE":node = handleDate(contents[i]);break;
+                case "FLOAT": case "DOUBLE": case "DECIMAL": node = handleFloat(contents[i]);break;
+                case "INTEGER": case "INT": case "BIGINT":node = handleInteger(contents[i]);break;
+                case "BOOLEAN": case "BOOL":node = handleBoolean(contents[i]);break;
+                default:throw new RuntimeException("Typ wird nicht unterstützt");
+            }
+            this.contentNodes.add(node);
+
+            this.add(dataDeletes.get(i),1,(2+i));
+            this.add(contentNodes.get(i),2,(2+i));
         }
     }
 
-    private void handleString(String content){
-        this.content = new TextField(content);
+    private Node handleText(String content){
+        return new TextField(content);
     }
 
-    private void HandleInteger(String content){
-        this.content = new TextField(content);
+    private Node handleDate(String content) {
+        LocalDate date;
+        try{
+            date = LocalDate.parse(content);
+        } catch (DateTimeParseException e){
+            throw new RuntimeException("Given type and content do not match");
+        }
+        return new DatePicker(date);
+    }
+
+
+    private Node handleInteger(String content){
+        return new TextField(content);
+    }
+
+    private Node handleBoolean(String content){
+        CheckBox checkBox= new CheckBox();
+        checkBox.setSelected(Boolean.parseBoolean(content));
+        return checkBox;
+    }
+
+    private Node handleFloat(String content){
+        return new TextField(content);
     }
 }
