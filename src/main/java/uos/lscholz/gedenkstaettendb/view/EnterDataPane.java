04d30915342664f9f31c79ebb7db9c4db6fe65a7
Lena Scholz <lscholz@uos.de>
2018-03-20 11:35:27 +0100

    Start giving EnterDataPane some content

--- src/main/java/uos/lscholz/gedenkstaettendb/view/EnterDataPane.java
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/EnterDataPane.java
@@ -1,10 +1,15 @@
 package uos.lscholz.gedenkstaettendb.view;
 
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.GridPane;
 
 public class EnterDataPane extends GridPane {
 
+    private ScrollPane scrollPane;
+    private GridPane gridPane;
+
     public EnterDataPane(){
+
 
     }
 }
