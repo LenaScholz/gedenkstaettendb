46968ffa1958f6acbdacbb8647a4ae0127c4404b
Lena Scholz <lscholz@uos.de>
2018-04-09 11:41:12 +0200

    Create DataPane Subclasses and move to individual package for folder structure that is easier to navigate

---
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/datapanes/FloatDataPane.java
@@ -0,0 +1,12 @@
+package uos.lscholz.gedenkstaettendb.view.datapanes;
+
+public class FloatDataPane extends DataPane{
+    /**
+     * @param label    Name of the datafield
+     * @param type     Type of the data stored in the datafield
+     * @param contents Array of the data already stored for that datafield
+     */
+    public FloatDataPane(String label, String type, String[] contents) {
+        super(label, type, contents);
+    }
+}
