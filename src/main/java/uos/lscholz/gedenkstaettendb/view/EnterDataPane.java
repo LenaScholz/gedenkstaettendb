f9db00acae8876942e71ae183c022e25a7833833
Lena Scholz <lscholz@uos.de>
2018-04-09 12:31:18 +0200

    Adapt EnterDataPane to new logic of DataPane creation

--- src/main/java/uos/lscholz/gedenkstaettendb/view/EnterDataPane.java
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/EnterDataPane.java
@@ -4,7 +4,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.GridPane;
 import uos.lscholz.gedenkstaettendb.model.SampleSource;
-import uos.lscholz.gedenkstaettendb.view.datapanes.DataPane;
+import uos.lscholz.gedenkstaettendb.view.datapanes.*;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -32,7 +32,7 @@ public class EnterDataPane extends GridPane {
         SampleSource source = new SampleSource();
         int i = 0; //Todo: do this prettier
         for(Map.Entry<String,String> entry : source.datatypes.entrySet()){
-            DataPane dataPane = new DataPane(entry.getKey(),entry.getValue(),source.contents.get(entry.getKey()));
+            DataPane dataPane = this.createDataPane(entry.getKey(),entry.getValue(),source.contents.get(entry.getKey()));
             this.datapanes.add(dataPane);
             this.gridPane.add(dataPane,1,i++);
             this.gridPane.add(new Separator(),1,i++);
@@ -43,4 +43,35 @@ public class EnterDataPane extends GridPane {
         this.add(this.scrollPane,1,1);
 
     }
+
+    /**
+     * TODO: complete proper Factory
+     * @param label
+     * @param type
+     * @param contents
+     * @return
+     */
+    private DataPane createDataPane(String label, String type, String [] contents) {
+        DataPane dataPane = null;
+        switch (type) {
+            case "TEXT":
+                return new TextDataPane(label, type, contents);
+            case "DATE":
+                return new DateDataPane(label, type, contents);
+            case "FLOAT":
+            case "DOUBLE":
+            case "DECIMAL":
+                return new FloatDataPane(label, type, contents);
+            case "INTEGER":
+            case "INT":
+            case "BIGINT":
+                return new IntegerDataPane(label, type, contents);
+            case "BOOLEAN":
+            case "BOOL":
+                return new BooleanDataPane(label, type, contents);
+            default:
+                throw new RuntimeException("Typ wird nicht unterstützt");
+        }
+    }
 }
+
