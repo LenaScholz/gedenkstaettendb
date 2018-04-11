d5faefc4764a0589f631084da59cc90bef5699de
Lena Scholz <lscholz@uos.de>
2018-04-11 11:09:16 +0200

    Create Factory to generate DataPanes

--- src/main/java/uos/lscholz/gedenkstaettendb/view/EnterDataPane.java
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/EnterDataPane.java
@@ -32,7 +32,7 @@ public class EnterDataPane extends GridPane {
         SampleSource source = new SampleSource();
         int i = 0; //Todo: do this prettier
         for(Map.Entry<String,String> entry : source.datatypes.entrySet()){
-            DataPane dataPane = this.createDataPane(entry.getKey(),entry.getValue(),source.contents.get(entry.getKey()));
+            DataPane dataPane = DataPaneFactory.createDataPane(entry.getKey(),entry.getValue(),source.contents.get(entry.getKey()));
             this.datapanes.add(dataPane);
             this.gridPane.add(dataPane,1,i++);
             this.gridPane.add(new Separator(),1,i++);
@@ -44,34 +44,5 @@ public class EnterDataPane extends GridPane {
 
     }
 
-    /**
-     * TODO: complete proper Factory
-     * @param label
-     * @param type
-     * @param contents
-     * @return
-     */
-    private DataPane createDataPane(String label, String type, String [] contents) {
-        DataPane dataPane = null;
-        switch (type) {
-            case "TEXT":
-                return new TextDataPane(label, type, contents);
-            case "DATE":
-                return new DateDataPane(label, type, contents);
-            case "FLOAT":
-            case "DOUBLE":
-            case "DECIMAL":
-                return new FloatDataPane(label, type, contents);
-            case "INTEGER":
-            case "INT":
-            case "BIGINT":
-                return new IntegerDataPane(label, type, contents);
-            case "BOOLEAN":
-            case "BOOL":
-                return new BooleanDataPane(label, type, contents);
-            default:
-                throw new RuntimeException("Typ wird nicht unterstützt");
-        }
-    }
 }
 
---
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/datapanes/DataPaneFactory.java
@@ -0,0 +1,33 @@
+package uos.lscholz.gedenkstaettendb.view.datapanes;
+
+public class DataPaneFactory {
+
+    /**
+     * @param label
+     * @param type
+     * @param contents
+     * @return
+     */
+    public static DataPane createDataPane(String label, String type, String [] contents) {
+        DataPane dataPane = null;
+        switch (type) {
+            case "TEXT":
+                return new TextDataPane(label, type, contents);
+            case "DATE":
+                return new DateDataPane(label, type, contents);
+            case "FLOAT":
+            case "DOUBLE":
+            case "DECIMAL":
+                return new FloatDataPane(label, type, contents);
+            case "INTEGER":
+            case "INT":
+            case "BIGINT":
+                return new IntegerDataPane(label, type, contents);
+            case "BOOLEAN":
+            case "BOOL":
+                return new BooleanDataPane(label, type, contents);
+            default:
+                throw new RuntimeException("Typ wird nicht unterstützt");
+        }
+    }
+}
