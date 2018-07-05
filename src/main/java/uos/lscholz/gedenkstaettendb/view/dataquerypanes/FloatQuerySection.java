6cb3ff9cbee3787005dd549811aede6fa8abd4a7
Lena Scholz <lscholz@uos.de>
2018-07-05 23:26:45 +0200

    fill FloatQuerySection

--- src/main/java/uos/lscholz/gedenkstaettendb/view/dataquerypanes/FloatQuerySection.java
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/dataquerypanes/FloatQuerySection.java
@@ -1,12 +1,28 @@
 package uos.lscholz.gedenkstaettendb.view.dataquerypanes;
 
+import javafx.scene.control.TextField;
+
 public class FloatQuerySection extends QuerySection {
 
+    private TextField lowerFloat;
+    private TextField upperFloat;
+
     public FloatQuerySection (){
+        this(null);
 
     }
 
     public FloatQuerySection(String ... contents){
+        this.lowerFloat = new TextField();
+        this.upperFloat = new TextField();
+        switch (contents.length){
+            case 0: break;
+            case 2: this.upperFloat.setText(contents[1]);
+            case 1: this.lowerFloat.setText(contents[0]);break;
+            default: throw new RuntimeException("Too many arguments in contents");
+        }
+        this.add(lowerFloat,1,1);
+        this.add(upperFloat,2,1);
 
     }
 
