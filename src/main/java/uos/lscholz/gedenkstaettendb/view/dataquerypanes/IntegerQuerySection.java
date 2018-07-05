5ef7b04b4d80981f01093996ee2059ab04c87d84
Lena Scholz <lscholz@uos.de>
2018-07-05 23:30:56 +0200

    fill IntegerQuerySection

---
+++ src/main/java/uos/lscholz/gedenkstaettendb/view/dataquerypanes/IntegerQuerySection.java
@@ -0,0 +1,30 @@
+package uos.lscholz.gedenkstaettendb.view.dataquerypanes;
+
+import javafx.scene.control.TextField;
+
+public class IntegerQuerySection extends QuerySection{
+    private TextField lowerInt;
+    private TextField upperInt;
+    public IntegerQuerySection(){
+        this(null);
+    }
+
+    public IntegerQuerySection(String ... contents){
+        this.lowerInt = new TextField();
+        this.upperInt = new TextField();
+        switch (contents.length){
+            case 0: break;
+            case 2: this.upperInt.setText(contents[1]);
+            case 1: this.lowerInt.setText(contents[0]);break;
+            default: throw new RuntimeException("Too many arguments in contents");
+        }
+        this.add(lowerInt,1,1);
+        this.add(upperInt,2,1);
+
+    }
+
+    @Override
+    protected String getHelpInfo() {
+        return null;
+    }
+}
